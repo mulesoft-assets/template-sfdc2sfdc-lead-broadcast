101aa88d30267accc6e92c36da5cb939b4a79ccb
Aurel Medvegy <aurel.medvegy@hotovo.org>
2014-05-30 15:30:37 +0200

    Integration test has implemented.

---
+++ src/test/java/org/mule/templates/integration/AbstractTemplateTestCase.java
@@ -0,0 +1,113 @@
+package org.mule.templates.integration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Date;
+import java.util.Map;
+import java.util.Properties;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.mule.MessageExchangePattern;
+import org.mule.api.MuleEvent;
+import org.mule.api.config.MuleProperties;
+import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.tck.probe.PollingProber;
+import org.mule.tck.probe.Prober;
+import org.mule.templates.test.utils.PipelineSynchronizeListener;
+import org.mule.transport.NullPayload;
+
+/**
+ * This is the base test class for Anypoint Template integration tests.
+ * 
+ * @author damiansima
+ */
+public class AbstractTemplateTestCase extends FunctionalTestCase {
+	private static final String MAPPINGS_FOLDER_PATH = "./mappings";
+	private static final String TEST_FLOWS_FOLDER_PATH = "./src/test/resources/flows/";
+	private static final String MULE_DEPLOY_PROPERTIES_PATH = "./src/main/app/mule-deploy.properties";
+
+	protected static final int TIMEOUT_SEC = 300;
+	protected static final String POLL_FLOW_NAME = "triggerFlow";
+	protected static final String TEMPLATE_NAME = "lead-broadcast";
+
+	protected final Prober pollProber = new PollingProber(60000, 1000l);
+	protected final PipelineSynchronizeListener pipelineListener = new PipelineSynchronizeListener(POLL_FLOW_NAME);
+
+	protected SubflowInterceptingChainLifecycleWrapper retrieveLeadFromBFlow;
+
+	@Rule
+	public DynamicPort port = new DynamicPort("http.port");
+	
+	
+	@Override
+	protected String getConfigResources() {
+		String resources = "";
+		try {
+			Properties props = new Properties();
+			props.load(new FileInputStream(MULE_DEPLOY_PROPERTIES_PATH));
+			resources = props.getProperty("config.resources");
+		} catch (Exception e) {
+			throw new IllegalStateException(
+					"Could not find mule-deploy.properties file on classpath. Please add any of those files or override the getConfigResources() method to provide the resources by your own.");
+		}
+
+		return resources + getTestFlows();
+	}
+
+	protected String getTestFlows() {
+		StringBuilder resources = new StringBuilder();
+
+		File testFlowsFolder = new File(TEST_FLOWS_FOLDER_PATH);
+		File[] listOfFiles = testFlowsFolder.listFiles();
+		if (listOfFiles != null) {
+			for (File f : listOfFiles) {
+				if (f.isFile() && f.getName().endsWith("xml")) {
+					resources.append(",").append(TEST_FLOWS_FOLDER_PATH).append(f.getName());
+				}
+			}
+			return resources.toString();
+		} else {
+			return "";
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected Map<String, Object> invokeRetrieveFlow(SubflowInterceptingChainLifecycleWrapper flow, Map<String, Object> payload) throws Exception {
+		MuleEvent event = flow.process(getTestEvent(payload, MessageExchangePattern.REQUEST_RESPONSE));
+		Object resultPayload = event.getMessage()
+									.getPayload();
+
+		if (resultPayload instanceof NullPayload) {
+			return null;
+		} else {
+			return (Map<String, Object>) resultPayload;
+		}
+	}
+
+	@Override
+	protected Properties getStartUpProperties() {
+		Properties properties = new Properties(super.getStartUpProperties());
+
+		String pathToResource = MAPPINGS_FOLDER_PATH;
+		File graphFile = new File(pathToResource);
+
+		properties.put(MuleProperties.APP_HOME_DIRECTORY_PROPERTY, graphFile.getAbsolutePath());
+
+		return properties;
+	}
+
+	protected String buildUniqueName(String templateName, String name) {
+		String timeStamp = new Long(new Date().getTime()).toString();
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(name);
+		builder.append(templateName);
+		builder.append(timeStamp);
+
+		return builder.toString();
+	}
+}
---
+++ src/test/java/org/mule/templates/integration/BusinessLogicIT.java
@@ -0,0 +1,152 @@
+package org.mule.templates.integration;
+
+import static org.junit.Assert.assertEquals;
+import static org.mule.templates.builders.SfdcObjectBuilder.anLead;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mule.MessageExchangePattern;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.context.notification.NotificationException;
+import org.mule.processor.chain.SubflowInterceptingChainLifecycleWrapper;
+import org.mule.templates.test.utils.ListenerProbe;
+
+import com.mulesoft.module.batch.BatchTestHelper;
+import com.sforce.soap.partner.SaveResult;
+
+/**
+ * The objective of this class is to validate the correct behavior of the
+ * Anypoint Template that make calls to external systems.
+ * 
+ */
+public class BusinessLogicIT extends AbstractTemplateTestCase {
+
+	private BatchTestHelper helper;
+	private List<Map<String, Object>> createdLeadsInA = new ArrayList<Map<String, Object>>();
+	private List<Map<String, Object>> createdLeadInB = new ArrayList<Map<String, Object>>();
+
+	private SubflowInterceptingChainLifecycleWrapper createLeadInAFlow;
+	private SubflowInterceptingChainLifecycleWrapper createLeadInBFlow;
+	
+	private SubflowInterceptingChainLifecycleWrapper deleteLeadFromAflow;
+	private SubflowInterceptingChainLifecycleWrapper deleteLeadFromBflow;
+	
+	@BeforeClass
+	public static void init() {
+		// Set the frequency between polls to 10 seconds
+		System.setProperty("polling.frequency", "10000");
+
+		// Set the poll starting delay to 20 seconds
+		System.setProperty("polling.startDelayMillis", "20000");
+	}
+	@Before
+	public void setUp() throws Exception {
+		stopFlowSchedulers(POLL_FLOW_NAME);
+		registerListeners();
+
+		helper = new BatchTestHelper(muleContext);
+
+		// Flow to retrieve accounts from target system after syncing
+		retrieveLeadFromBFlow = getSubFlow("retrieveLeadFlow");
+		retrieveLeadFromBFlow.initialise();
+
+		// Create object in target system to be update
+		createLeadInBFlow = getSubFlow("createLeadFlowB");
+		createLeadInBFlow.initialise();
+
+		// Create accounts in source system to be or not to be synced
+		createLeadInAFlow = getSubFlow("createLeadFlowA");
+		createLeadInAFlow.initialise();
+
+		// Delete the created accounts in A
+		deleteLeadFromAflow = getSubFlow("deleteLeadFromAFlow");
+		deleteLeadFromAflow.initialise();
+
+		// Delete the created accounts in B
+		deleteLeadFromBflow = getSubFlow("deleteLeadFromBFlow");
+		deleteLeadFromBflow.initialise();
+
+		createEntities();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		stopFlowSchedulers(POLL_FLOW_NAME);
+		deleteEntities(createdLeadsInA);
+		deleteEntities(createdLeadInB);
+	}
+
+	@Test
+	public void testMainFlow() throws Exception {
+		// Run poll and wait for it to run
+		runSchedulersOnce(POLL_FLOW_NAME);
+		waitForPollToRun();
+
+		// Wait for the batch job executed by the poll flow to finish
+		helper.awaitJobTermination(TIMEOUT_SEC * 1000, 500);
+		helper.assertJobWasSuccessful();
+
+		// Assert first object was sync to target system
+		Map<String, Object> payload = invokeRetrieveFlow(retrieveLeadFromBFlow, createdLeadsInA.get(0));
+		assertEquals("The account should have been sync", createdLeadsInA.get(0).get("Email"), payload.get("Email"));
+
+		// Assert fourth object was sync to target system
+		Map<String, Object> secondLead = createdLeadsInA.get(1);
+		payload = invokeRetrieveFlow(retrieveLeadFromBFlow, secondLead);
+		assertEquals("The account should have been sync (Email)", secondLead.get("Email"), payload.get("Email"));
+	}
+
+	private void registerListeners() throws NotificationException {
+		muleContext.registerListener(pipelineListener);
+	}
+
+	private void waitForPollToRun() {
+		pollProber.check(new ListenerProbe(pipelineListener));
+	}
+
+	@SuppressWarnings("unchecked")
+	private void createEntities() throws MuleException, Exception {
+
+		createdLeadInB.add(anLead().with("Email", buildUniqueName(TEMPLATE_NAME, "b.") + "@test.com").with("FirstName", "FirstName").with("LastName", "LastName").with("Country", "California").with("Company", "Good one Ltd.").build());
+		createLeadInBFlow.process(getTestEvent(createdLeadInB, MessageExchangePattern.REQUEST_RESPONSE));
+
+		createdLeadsInA.add(anLead().with("Email", buildUniqueName(TEMPLATE_NAME, "a1.") + "@test.com").with("FirstName", "FirstName1").with("LastName", "LastName1").with("Country", "California").with("Company", "Not so good one Ltd.").build());
+		createdLeadsInA.add(anLead().with("Email", buildUniqueName(TEMPLATE_NAME, "a2.") + "@test.com").with("FirstName", "FirstName2").with("LastName", "LastName2").with("Country", "North Dakota").with("Company", "Best one Ltd.").build());
+
+		final MuleEvent event = createLeadInAFlow.process(getTestEvent(createdLeadsInA, MessageExchangePattern.REQUEST_RESPONSE));
+		final List<SaveResult> results = (List<SaveResult>) event.getMessage().getPayload();
+		int i = 0;
+		for (SaveResult result : results) {
+			Map<String, Object> accountInA = createdLeadsInA.get(i);
+			accountInA.put("Id", result.getId());
+			i++;
+		}
+	}
+
+	private void deleteEntities(List<Map<String, Object>> leads) throws MuleException, Exception {
+
+		final List<Object> idList = new ArrayList<Object>();
+		for (final Map<String, Object> c : leads) {
+			idList.add(c.get("Id"));
+		}
+		deleteLeadFromAflow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+
+
+		idList.clear();
+		for (final Map<String, Object> createdLead : leads) {
+			final Map<String, Object> account = invokeRetrieveFlow(retrieveLeadFromBFlow, createdLead);
+			if (account != null) {
+				idList.add(account.get("Id"));
+			}
+		}
+		deleteLeadFromBflow.process(getTestEvent(idList, MessageExchangePattern.REQUEST_RESPONSE));
+	}
+
+}
